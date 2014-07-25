9f1ebbcb0b07b4f2bc6d8a89965c42836a41f602
Benjamin Hendricks <benjaminhendricks@kirbymaster.linkedin.biz>
2014-07-25 10:44:20 -0700

    added text

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('Hello Everyone! GymBud is currently under construction, please come back soon! With pressing needs, please send an email to ben@gymbudapp.com...Thanks!');
   });
 
   var port = Number(process.env.PORT || 5000);
